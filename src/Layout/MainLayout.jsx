e92b4460c77449b24302d0c986da3f922079dec0
Rummaninfo <birsabbir38@gmail.com>
2025-11-09 00:17:57 -0600

    client site setup

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navbar from '../Component/Navbar';
+import { Outlet } from 'react-router';
+import Footer from '../Component/Footer';
+
+const MainLayout = () => {
+    return (
+        <div className='flex flex-col min-h-screen'>
+            <header>
+                <Navbar></Navbar>
+            </header>
+            <main className=' container mx-auto flex-1'>
+                <Outlet></Outlet>
+            </main>
+            <footer>
+                <Footer></Footer>
+            </footer>
+           
+        </div>
+    );
+};
+
+export default MainLayout;
